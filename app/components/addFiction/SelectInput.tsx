73180cdc3c97336f7190ec06c979dd7cbb08145e
denniztr <denniztr@gmail.com>
2024-05-12 21:48:26 +0300

    transition for pop-up, ts error needs to be fixed

--- app/components/addFiction/SelectInput.tsx
+++ app/components/addFiction/SelectInput.tsx
@@ -2,18 +2,18 @@
 
 
 import { useState } from 'react';
-
-import Input from '@/app/components/ui/inputs/addFictionInput';
-
+import { Transition } from '@headlessui/react';
 import { IoIosArrowDown } from "react-icons/io";
 
+import Input from '@/app/components/ui/inputs/addFictionInput';
+import clsx from 'clsx';
 
 type SelectVariant = 'group' | 'fandom' | 'character' | 'relation' | 'access'
-
 
 interface SelectProps {
   placeholder?: string
   id: SelectVariant
+  ref?: TransitionChildRenderProp
 }
 
 const Select: React.FC<SelectProps> = ({ placeholder, id }) => {
@@ -22,14 +22,27 @@ const Select: React.FC<SelectProps> = ({ placeholder, id }) => {
   const handleClick = (id: SelectVariant) => {
     setIsOpen(isOpen === id ? null : id)
   }
-  console.log(isOpen)
+
   return (
-    <div className="w-full relative" id={id} onClick={() => handleClick(id)}>
+    <div className="w-full relative z-10" id={id} onClick={() => handleClick(id)}>
       <Input className='cursor-pointer' placeholder={placeholder}/>
-      <IoIosArrowDown size={23}  className='absolute top-1/2 -translate-y-1/2 right-3 text-gray-400 cursor-pointer'/>
-      {isOpen === id && (
-        <div className='absolute p-4 bg-gray-400 rounded-b w-full h-[100px] text-black'>список</div>
-      )}
+      <IoIosArrowDown size={23} className={clsx(`absolute top-1/2 -translate-y-1/2 right-3 text-gray-400 cursor-pointer transition-all duration-300 `, 
+        isOpen && '-rotate-180'
+        )}
+      />
+      <Transition
+          show={isOpen === id}
+          enter="transition ease-out duration-200"
+          enterFrom="opacity-0 translate-y-1"
+          enterTo="opacity-100 translate-y-0"
+          leave="transition ease-in duration-150"
+          leaveFrom="opacity-100 translate-y-0"
+          leaveTo="opacity-0 translate-y-1"
+        >
+            {(ref) => (
+              <div ref={ref} className='absolute p-4 bg-white rounded-b w-full h-[100px] text-black z-20'>список</div>
+            )}
+      </Transition>
     </div>
   )
 }
