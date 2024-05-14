0aca72a05d099377bfb4943e158a1d96ca984744
denniztr <denniztr@gmail.com>
2024-05-14 20:57:44 +0300

    chaging checkbox styles

--- app/components/addFiction/AddFicForm.tsx
+++ app/components/addFiction/AddFicForm.tsx
@@ -14,11 +14,9 @@ import ratings from '@/app/constants/rating';
 
 import clsx from 'clsx';
 
-
-type Authorship = 'author' | 'translation'; 
+type Authorship = 'author' | 'translation';
 type FictionType = 'original' | 'fandom';
 type Rating = 'g' | 'pg-13' | 'r' | 'nc-17' | 'nc-21';
-
 
 const AddFicForm = () => {
   const [authorship, setAuthorship] = useState<Authorship>('author');
@@ -31,7 +29,6 @@ const AddFicForm = () => {
   const [chosenGroup, setChosenGroup] = useState<string>('');
   const [chosenFandom, setChosenFandom] = useState<string>('');
 
-  
   const {
     register,
     handleSubmit,
@@ -58,42 +55,47 @@ const AddFicForm = () => {
   });
 
   const onSubmit: SubmitHandler<FieldValues> = (values) => {
-    console.log(values)
-  }
+    console.log(values);
+  };
 
-
-  const toggleRadioButton = (value: Authorship | FictionType | Rating, name: string) => {
+  const toggleRadioButton = (
+    value: Authorship | FictionType | Rating,
+    name: string
+  ) => {
     if (name === 'authorship') {
-      setAuthorship(value as Authorship)
+      setAuthorship(value as Authorship);
     } else if (name === 'type') {
-      setFictionType(value as FictionType)
+      setFictionType(value as FictionType);
     } else if (name === 'rating') {
-      setSelectedRating(value as Rating)
+      setSelectedRating(value as Rating);
     }
-  }
+  };
 
   return (
     <div className="w-full py-6 md:pr-6 md:pl-24">
-      <form className=" text-gray-300 space-y-6" onSubmit={handleSubmit(onSubmit)}>
+      <form
+        className=" text-gray-300 space-y-6"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className="flex gap-6">
           <label>Авторство</label>
           <div className="flex flex-col">
-            <RadioInput 
-              register={register} 
-              name="authorship" 
-              value='author'
+            <RadioInput
+              register={register}
+              name="authorship"
+              value="author"
               checked={authorship === 'author'}
-              handleClick={toggleRadioButton} 
+              handleClick={toggleRadioButton}
               readOnly
             >
               Работа Вашего авторства
             </RadioInput>
-            <RadioInput 
-              register={register} 
-              name="authorship" 
-              value='translation'
+            <RadioInput
+              register={register}
+              name="authorship"
+              value="translation"
               checked={authorship === 'translation'}
-              handleClick={toggleRadioButton} 
+              handleClick={toggleRadioButton}
               readOnly
             >
               Перевод с другого языка (с разрешения автора оригинала)
@@ -103,23 +105,39 @@ const AddFicForm = () => {
         {authorship !== 'author' && (
           <>
             <div className="space-y-2">
-              <label>Имя автора <span className='text-red-600'>*</span></label>
+              <label>
+                Имя автора <span className="text-red-600">*</span>
+              </label>
               <div className="w-full">
-                <TextInput placeholder='Имя автора оригинала' register={register} id='author' />
+                <TextInput
+                  placeholder="Имя автора оригинала"
+                  register={register}
+                  id="author"
+                />
               </div>
+            </div>
+            <div className="space-y-2">
+              <label className="text-sm">
+                Ссылка на оригинал <span className="text-red-600">*</span>
+              </label>
+              <div className="w-full">
+                <TextInput
+                  placeholder="https://ссылка"
+                  register={register}
+                  id="link"
+                />
               </div>
-              <div className="space-y-2">
-                <label className='text-sm'>Ссылка на оригинал  <span className='text-red-600'>*</span></label>
-                <div className="w-full">
-                  <TextInput placeholder='https://ссылка' register={register} id='link' />
-                </div>
-              </div>
+            </div>
           </>
         )}
         <div className="space-y-2">
           <label>Название</label>
           <div className="w-full">
-            <TextInput placeholder='Название произведения' register={register} id='title' />
+            <TextInput
+              placeholder="Название произведения"
+              register={register}
+              id="title"
+            />
           </div>
         </div>
         <div className="flex gap-6">
@@ -131,92 +149,96 @@ const AddFicForm = () => {
               <Button>Гамму</Button>
             </div>
             <div>
-              <MultipleDataInput 
-                placeholder='Начните вводить имя' 
-                id='collaborators'
+              <MultipleDataInput
+                placeholder="Начните вводить имя"
+                id="collaborators"
                 setValue={setValue}
               />
             </div>
           </div>
         </div>
         <div className="flex gap-6">
-          <label className='w-20'>Тип работы</label>
+          <label className="w-20">Тип работы</label>
           <div className="flex flex-col">
-            <RadioInput 
-                register={register} 
-                name="type" 
-                value='original'
-                checked={fictionType === 'original'}
-                handleClick={toggleRadioButton} 
-                readOnly
-              >
-                Ориджинал
-              </RadioInput>
-              <RadioInput 
-                register={register} 
-                name="type" 
-                value='fandom'
-                checked={fictionType === 'fandom'}
-                handleClick={toggleRadioButton} 
-                readOnly
-              >
-                Фанфик по фандому
-              </RadioInput>
+            <RadioInput
+              register={register}
+              name="type"
+              value="original"
+              checked={fictionType === 'original'}
+              handleClick={toggleRadioButton}
+              readOnly
+            >
+              Ориджинал
+            </RadioInput>
+            <RadioInput
+              register={register}
+              name="type"
+              value="fandom"
+              checked={fictionType === 'fandom'}
+              handleClick={toggleRadioButton}
+              readOnly
+            >
+              Фанфик по фандому
+            </RadioInput>
           </div>
         </div>
-        <div className={clsx(`space-y-2`, fictionType === 'fandom' && 'hidden')}>
+        <div
+          className={clsx(`space-y-2`, fictionType === 'fandom' && 'hidden')}
+        >
           <label>Персонажи</label>
-          <MultipleDataInput 
-            placeholder='Добавляйте персонажей по одному' 
-            id='characters'
+          <MultipleDataInput
+            placeholder="Добавляйте персонажей по одному"
+            id="characters"
             setValue={setValue}
           />
         </div>
         {fictionType === 'fandom' && (
           <>
             <div className="space-y-2">
-              <label className='text-sm'>Выберите фандом  <span className='text-red-600'>*</span></label>
-              <SelectInput 
-                placeholder='Выберите группу'
+              <label className="text-sm">
+                Выберите фандом <span className="text-red-600">*</span>
+              </label>
+              <SelectInput
+                placeholder="Выберите группу"
                 setValue={setValue}
                 register={register}
-                id='group'
+                id="group"
                 state={chosenGroup}
                 dispatch={setChosenGroup}
               />
-              <SelectInput 
-                placeholder='Укажите фандом'
+              <SelectInput
+                placeholder="Укажите фандом"
                 setValue={setValue}
                 register={register}
-                id='fandom'
+                id="fandom"
                 state={chosenFandom}
                 dispatch={setChosenFandom}
               />
             </div>
             <div className="space-y-2">
               <label>Персонажи</label>
-              <SelectInput 
-                placeholder='Добавляйте персонажей по одному'
+              <SelectInput
+                placeholder="Добавляйте персонажей по одному"
                 setValue={setValue}
                 register={register}
-                id='character'
+                id="character"
                 state={chosenCharacter}
                 dispatch={setChosenCharacter}
               />
             </div>
           </>
-        )} 
-         <div className="flex gap-6">
-          <label className='w-20'>Рейтинг</label>
+        )}
+        <div className="flex gap-6">
+          <label className="w-20">Рейтинг</label>
           <div className="flex flex-col">
             {ratings.map((rating, index) => (
-              <RadioInput 
-                key={index} 
+              <RadioInput
+                key={index}
                 register={register}
-                name="rating" 
+                name="rating"
                 value={rating.label}
                 checked={rating.label === selectedRating}
-                handleClick={toggleRadioButton} 
+                handleClick={toggleRadioButton}
                 readOnly
               >
                 {rating.label.toUpperCase()}
@@ -226,20 +248,20 @@ const AddFicForm = () => {
         </div>
         <div className="space-y-2">
           <label>Отношения</label>
-          <SelectInput 
+          <SelectInput
             setValue={setValue}
-            placeholder='Выберите отношения' 
-            id='relation' 
+            placeholder="Выберите отношения"
+            id="relation"
             register={register}
-            state={chosenRelation} 
-            dispatch={setChosenRelation} 
+            state={chosenRelation}
+            dispatch={setChosenRelation}
           />
-        </div> 
+        </div>
         <div className="space-y-2">
           <label>Метки</label>
-          <MultipleDataInput 
-            placeholder='Начните вводить...' 
-            id='tags'
+          <MultipleDataInput
+            placeholder="Начните вводить..."
+            id="tags"
             setValue={setValue}
           />
         </div>
@@ -247,10 +269,7 @@ const AddFicForm = () => {
           <label>Короткое описание работы</label>
           <div className="w-full">
             <div>
-              <Textarea 
-                register={register}
-                id='description'
-              />
+              <Textarea register={register} id="description" />
             </div>
           </div>
         </div>
@@ -258,31 +277,63 @@ const AddFicForm = () => {
           <label>Примечания</label>
           <div className="w-full">
             <div>
-              <Textarea 
-                register={register}
-                id='notes'
-              />
+              <Textarea register={register} id="notes" />
             </div>
           </div>
         </div>
         <div className="space-y-2">
           <label>Разрешение на публикацию</label>
-          <SelectInput 
-            placeholder='Варианты'
+          <SelectInput
+            placeholder="Варианты"
             setValue={setValue}
             register={register}
-            id='access'
+            id="access"
             state={chosenAccessRules}
             dispatch={setChosenAccessRules}
           />
-          <span className='text-xs text-gray-400'>Разрешение публикации на других ресурсах.</span>
-        </div> 
-        <div className='w-full text-center space-y-6'>
-          <div>
-            <input type="checkbox" className="h-4 w-4"/>
-            <span className='pl-2 text-sm text-gray-400'>Я подтверждаю, что публикуемый перевод выполнен мной с иностранного языка, а разрешение на перевод получено у автора, и понимаю, что публикация чужих работ может привести к блокировке доступа на сайт.</span>
+          <span className="text-xs text-gray-400">
+            Разрешение публикации на других ресурсах.
+          </span>
+        </div>
+        <div className="w-full text-center space-y-6">
+          <div className="flex">
+            <label className="">
+              <input
+                className="peer cursor-pointer hidden after:opacity-100"
+                type="checkbox"
+              />
+              <span
+                className="
+                    inline-block 
+                    w-5 
+                    h-5 
+                    border-2 
+                    relative 
+                    cursor-pointer 
+                    after:content-[''] 
+                    after:absolute 
+                    after:top-2/4 
+                    after:left-2/4 
+                    after:-translate-x-1/2 
+                    after:-translate-y-1/2 
+                    after:w-[10px] 
+                    after:h-[10px] 
+                    after:bg-white 
+                    after:rounded-[2px] 
+                    after:opacity-0 
+                    peer-checked:after:opacity-100"
+              ></span>
+            </label>
+            <span className="pl-2 text-sm text-gray-400">
+              Я подтверждаю, что публикуемый перевод выполнен мной с
+              иностранного языка, а разрешение на перевод получено у автора, и
+              понимаю, что публикация чужих работ может привести к блокировке
+              доступа на сайт.
+            </span>
           </div>
-          <Button disabled={false} type='submit'>Сохранить и перейти к публикации</Button>
+          <Button disabled={false} type="submit">
+            Сохранить и перейти к публикации
+          </Button>
         </div>
       </form>
     </div>
