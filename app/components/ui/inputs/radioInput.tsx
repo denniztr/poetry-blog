4443bbebd676505cb76c8417744750dc1260ed0d
denniztr <denniztr@gmail.com>
2024-05-14 20:41:31 +0300

    changing radio buttons styles, the form is ready for working with db

--- app/components/ui/inputs/radioInput.tsx
+++ app/components/ui/inputs/radioInput.tsx
@@ -2,15 +2,14 @@
 
 import { UseFormRegister, FieldValues } from 'react-hook-form';
 
-
-type Authorship = 'author' | 'translation'; 
+type Authorship = 'author' | 'translation';
 type FictionType = 'original' | 'fandom';
 type Rating = 'g' | 'pg-13' | 'r' | 'nc-17' | 'nc-21';
 
 interface RadioInputProps {
   children: React.ReactNode;
   register: UseFormRegister<FieldValues>;
-  name: string
+  name: string;
   value: string;
   readOnly?: boolean;
   checked: boolean;
@@ -24,21 +23,39 @@ const RadioInput: React.FC<RadioInputProps> = ({
   value,
   readOnly,
   checked,
-  handleClick
+  handleClick,
 }) => {
   return (
-    <label>
-      <input 
-        className='mr-2'
-        type="radio" 
+    <label className="relative flex items-center cursor-pointer">
+      <input
+        className="mr-2 sr-only peer"
+        type="radio"
         {...register(name, { required: true })}
         name={name}
         value={value}
         readOnly={readOnly}
         checked={checked}
-        onClick={() => handleClick(value as Authorship | FictionType | Rating, name)}
+        onClick={() =>
+          handleClick(value as Authorship | FictionType | Rating, name)
+        }
       />
-      {children}
+      <div
+        className="
+          w-4 
+          h-4 
+          bg-transparent 
+          border-2 
+          border-gray-400
+          rounded-full
+         peer-checked:bg-gray-400
+         peer-checked:border-gray-400
+          peer-hover:shadow-sm 
+        peer-hover:shadow-gray-400
+          transition 
+          duration-300 
+          ease-in-out"
+      />
+      <span className="ml-2">{children}</span>
     </label>
   );
 };
