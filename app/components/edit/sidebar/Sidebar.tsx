afe5d0194414a61a978ea12eea67f696157e954a
denniztr <denniztr@gmail.com>
2024-05-17 21:52:55 +0300

    dispatch via click

--- app/components/edit/sidebar/Sidebar.tsx
+++ app/components/edit/sidebar/Sidebar.tsx
@@ -1,15 +1,21 @@
 'use client'
 
+import { useState } from "react";
 
 import nav from "@/app/constants/editFictionNavigation";
 import SidebarItem from '@/app/components/edit/sidebar/ui/SidebarItem';
 
+
 const Sidebar = () => {
+  const [variant, setVariant] = useState<string>('ADD_PART');
+
+  console.log(variant)
+  
   return (
     <aside className="sticky top-3 border border-gray-700/30 bg-black/40 w-full md:w-[260px] h-[342px]">
       <ul className='divide-y-[1px] divide-gray-700/50 text-gray-400'>
         {nav.map((item, index) => (
-          <SidebarItem key={index} {...item} />
+          <SidebarItem key={index} {...item} dispatch={setVariant} />
         ))}
       </ul>
     </aside>
--- app/components/edit/sidebar/ui/SidebarItem.tsx
+++ app/components/edit/sidebar/ui/SidebarItem.tsx
@@ -1,12 +1,15 @@
 'use client';
 
+import { SetStateAction } from "react";
+
 interface SidebarItemProps {
   label: string;
+  dispatch: React.Dispatch<SetStateAction<string>>
 }
 
-const SidebarItem: React.FC<SidebarItemProps> = ({ label }) => {
+const SidebarItem: React.FC<SidebarItemProps> = ({ label, dispatch }) => {
   return (
-    <li className="cursor-pointer transition-all duration-300 hover:bg-black/40 hover:text-gray-300">
+    <li className="cursor-pointer transition-all duration-300 hover:bg-black/40 hover:text-gray-300" onClick={() => dispatch(label)}>
       <span className="py-4 px-6 inline-block whitespace-nowrap">{label}</span>
     </li>
   );
