1a5239789a1bdac851a0afc66ee90ecb069b5d7a
denniztr <denniztr@gmail.com>
2024-05-19 13:56:55 +0300

    submenu init ui, animation

--- app/components/profileDrawer/ui/NavigationItem.tsx
+++ app/components/profileDrawer/ui/NavigationItem.tsx
@@ -1,19 +1,57 @@
 'use client';
 
+import React, { useState } from 'react';
 import Link from 'next/link';
+
+import { AnimatePresence, motion } from 'framer-motion';
+
+import clsx from 'clsx';
 
 interface NavigationItemProps {
   label: string;
-  // icon: any;
+  icon: any;
+  iconAfter?: any;
+  subMenuItems?: { label: string; href: string; }[]; 
 }
 
 const NavigationItem: React.FC<NavigationItemProps> = ({
   label,
-  // icon: Icon,
+  icon: Icon,
+  iconAfter: IconAfter,
+  subMenuItems,
 }) => {
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+
+  const handleSubMenuToggle = () => {
+    setIsSubMenuOpen(!isSubMenuOpen);
+  };
+
   return (
-    <li className="flex items-center gap-3 text-gray-800 px-4 py-3 sm:px-6 transition-all duration-300 hover:bg-gray-300/90 cursor-pointer">
-      <Link href="#">{label}</Link>
+    <li className={clsx(`text-gray-800 px-4 py-3 sm:px-6 transition-all duration-300 hover:bg-gray-300/90 cursor-pointer`, isSubMenuOpen && 'bg-gray-300/90')}>
+      <div className="flex items-center justify-between gap-6" onClick={handleSubMenuToggle}>
+        <div className="flex items-center gap-6">
+          <Icon className='h-5 w-5' />
+          <Link href="#">{label}</Link>
+        </div>
+        {IconAfter && <IconAfter className={clsx(`h-5 w-5`, isSubMenuOpen && 'rotate-180')}/>}
+      </div>
+      <AnimatePresence>
+        {isSubMenuOpen && subMenuItems && (
+          <motion.ul
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: 'auto', opacity: 1 }}
+            exit={{ height: 0, opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            className="overflow-hidden mt-2 pl-10"
+          >
+            {subMenuItems.map((item, index) => (
+              <li key={index} className="py-2 transition-all duration-300 hover:bg-gray-400/90">
+                <Link href={item.href} className="text-gray-600 hover:text-gray-800">{item.label}</Link>
+              </li>
+            ))}
+          </motion.ul>
+        )}
+      </AnimatePresence>
     </li>
   );
 };
--- app/constants/profileDrawerNavigation.ts
+++ app/constants/profileDrawerNavigation.ts
@@ -1,25 +1,56 @@
-import { CgProfile } from "react-icons/cg";
+import { CgProfile } from 'react-icons/cg';
+import { TbBrandMessenger } from 'react-icons/tb';
+import { BsPencil } from 'react-icons/bs';
+import { FaBook } from 'react-icons/fa';
+import { FaJediOrder } from 'react-icons/fa6';
+import { MdOutlineContactSupport } from 'react-icons/md';
+import { IoIosArrowDown } from 'react-icons/io';
 
 const nav = [
   {
     label: 'Мой профиль',
-    icon: CgProfile
+    icon: CgProfile,
   },
   {
-    label: 'Сообщения'
+    label: 'Сообщения',
+    icon: TbBrandMessenger,
   },
   {
-    label: 'Для авторов'
+    label: 'Для авторов',
+    icon: BsPencil,
+    iconAfter: IoIosArrowDown,
+    subMenuItems: [
+      { label: 'Вариант 1', href: '#1' },
+      { label: 'Вариант 2', href: '#2' },
+    ],
   },
   {
-    label: 'Для читателей'
+    label: 'Для читателей',
+    icon: FaBook,
+    iconAfter: IoIosArrowDown,
+    subMenuItems: [
+      { label: 'Вариант 1', href: '#1' },
+      { label: 'Вариант 2', href: '#2' },
+    ],
   },
   {
-    label: 'Заявки'
+    label: 'Заявки',
+    icon: FaJediOrder,
+    iconAfter: IoIosArrowDown,
+    subMenuItems: [
+      { label: 'Вариант 1', href: '#1' },
+      { label: 'Вариант 2', href: '#2' },
+    ],
   },
   {
-    label: 'Поддержка'
+    label: 'Поддержка',
+    icon: MdOutlineContactSupport,
+    iconAfter: IoIosArrowDown,
+    subMenuItems: [
+      { label: 'Вариант 1', href: '#1' },
+      { label: 'Вариант 2', href: '#2' },
+    ],
   },
-]
+];
 
 export default nav;
